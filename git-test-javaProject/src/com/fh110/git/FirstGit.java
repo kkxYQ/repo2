e99160e3da359b313c9bbe15138e13dbe8d89f22
yq <1358741391@qq.com>
2019-06-24 18:56:59 +0800

    本地仓库第一次提交

---
+++ git-test-javaProject/src/com/fh110/git/FirstGit.java
@@ -0,0 +1,9 @@
+package com.fh110.git;
+
+public class FirstGit {
+    public static void main(String[] args) {
+        System.out.println ("aaaa");
+        System.out.println ("bbbb");
+
+    }
+}
